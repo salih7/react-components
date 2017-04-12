d5668e1f42ce4553ab818a2ccc76eb40e5006a0a
Hack Reactor Student <student@hackreactor.com>
2017-04-12 12:25:26 -0700

    Add user click event handler

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
-var GroceryListItem = (props) => (
-  <ul>
-    <li>{props.list[0]}</li>
-    <li>{props.list[1]}</li>
-    <li>{props.list[2]}</li>
-  </ul>
-);
+// var TodoList = (props) => {
+
+//   // This function will be called when the first `<li>` below is clicked on
+//   // Notice that event handling functions receive an `event` object
+//   // We want to define it where it has access to `props`
+
+//   var onListItemClick = (event) => {
+//     console.log('I got clicked');
+//   };
+
+//   // Because we used curly braces with this arrow function
+//   // we have to write an explicit `return` statement
+//   return (
+//     <ul>
+//       <li onClick={onListItemClick}>{props.todos[0]}</li>
+//       <li>{props.todos[1]}</li>
+//       <li>{props.todos[2]}</li>
+//     </ul>
+//   );
+// }
+
+// var App = () => (
+//   <div>
+//     <h2>My Todo List</h2>
+//     <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/> // Here we are creating an instance of the `TodoList` component
+//   </div>
+// );
+
+// ReactDOM.render(<App />, document.getElementById('app'));
+
+var GroceryListItem = (props) => {
+  var onListItemClick = (event) => {
+    console.log('I got clicked');
+  };
+  return (
+    <ul>
+      <li onClick={onListItemClick}>{props.list[0]}</li>
+      <li>{props.list[1]}</li>
+      <li>{props.list[2]}</li>
+    </ul>
+  );
+};
 
 var GroceryList = () => (
   <div>
